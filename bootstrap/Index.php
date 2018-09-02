e5e22001a612c4c8db726fc38c095ab9c54de995
L-Chris <563303226@qq.com>
2018-09-02 11:13:12 +0800

    add: smarty

--- bootstrap/Index.php
+++ bootstrap/Index.php
@@ -1,6 +1,16 @@
 <?php
 class Bootstrap extends Yaf_Bootstrap_Abstract {
+
 	public function _initPlugin(Yaf_Dispatcher $dispatcher) {
 		$dispatcher->registerPlugin(new AuthenticationPlugin());
+	}
+
+	public function _initSmarty(Yaf_Dispatcher $dispatcher) {
+		$smarty = new Smarty_Adapter(null , Yaf_Application::app()->getConfig()->smarty);
+		$dispatcher->setView($smarty);
+	}
+
+	public function _initView(Yaf_Dispatcher $dispatcher) {
+		$dispatcher->disableView();
 	}
 }
--- controllers/Index.php
+++ controllers/Index.php
@@ -1,5 +1,4 @@
 <?php
-
 class IndexController extends Yaf_Controller_Abstract {
   public $actions = array(
     "index" => "actions/IndexAction.php"
